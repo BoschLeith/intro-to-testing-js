e424e776157266fd22560e37933e55c4ded82a75
Bosch Leith <bosch.leith@gmail.com>
2021-08-24 11:22:47 -0500

    add the second test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ function helloWorld() {
     return "Hello, World!";
 }
 
+// sayHello function
 function sayHello() {
-
+    return '';
 }
--- tests.js
+++ tests.js
@@ -14,8 +14,12 @@ describe('helloWorld', function() {
     });
 });
 
+// Unit tests for the sayHello function
 describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
     });
 });
